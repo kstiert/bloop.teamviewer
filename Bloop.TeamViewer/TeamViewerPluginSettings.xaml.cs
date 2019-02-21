c8605a37c6ae5cea9024801172539a87d0321bcb
Devin Stiert <devin.stiert@thelevelup.com>
2019-02-21 08:31:14 -0500

    Initial commit

---
+++ Bloop.TeamViewer/TeamViewerPluginSettings.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Bloop.GitHub
+{
+    /// <summary>
+    /// Interaction logic for GitHubPluginSettings.xaml
+    /// </summary>
+    public partial class TeamViewerPluginSettings : UserControl
+    {
+        public GitHubPluginSettings(IDictionary<string, IShell> shells)
+        {
+            InitializeComponent();
+            Token.Text = PluginSettings.Instance.Token;
+        }
+
+        private void Token_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            PluginSettings.Instance.Token = textBox.Text;
+            PluginSettings.Instance.Save();
+        }
+    }
+}
